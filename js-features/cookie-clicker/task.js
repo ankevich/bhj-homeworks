0c3e6d2ddc12e2159e8ae0f2d1948b57cfb35097
Annie Bauer <overheads165@gmail.com>
2022-01-19 17:27:28 +0300

    тернарный оператор

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -3,14 +3,15 @@ let counter = Number(document.getElementById("clicker__counter").innerHTML);
 const cookie = document.getElementById("cookie");
 cookie.onclick = function () {
   counter++;
-  changeSizes();
+  cookie.width == 200 ? (cookie.width = 300) : (cookie.width = 200); //   changeSizes();
+  //   cookie.width = cookie.width == 200 ? 300 : 200;
   document.getElementById("clicker__counter").innerHTML = counter;
 };
 
-let changeSizes = () => {
-  if (cookie.width == 200) {
-    cookie.width = 300;
-  } else {
-    cookie.width = 200;
-  }
-};
+// let changeSizes = () => {
+//   if (cookie.width == 200) {
+//     cookie.width = 300;
+//   } else {
+//     cookie.width = 200;
+//   }
+// };
