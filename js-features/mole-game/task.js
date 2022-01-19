36125cdc185d97b855ca44c64dcdd8ad15446955
Annie Bauer <overheads165@gmail.com>
2022-01-20 00:06:07 +0300

    готово

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -1,16 +1,34 @@
-let deadCounter = Number(document.getElementById("dead").innerHTML);
-let lostCounter = Number(document.getElementById("lost").innerHTML);
+let deadCounter = 0;
+let lostCounter = 0;
 
-let holes = Array.from(document.getElementsByClassName("hole"))
+let holes = Array.from(document.getElementsByClassName("hole"));
 
-holes.map(hole => {
+holes.map((hole) => {
   hole.onclick = () => {
     if (hole.classList.contains("hole_has-mole")) {
-        deadCounter++
-        document.getElementById("dead").innerHTML = deadCounter
+      deadCounter++;
+      document.getElementById("dead").innerHTML = deadCounter;
     } else {
-        lostCounter++
-        document.getElementById("lost").innerHTML = lostCounter
+      lostCounter++;
+      document.getElementById("lost").innerHTML = lostCounter;
     }
+    checkVictory();
   };
 });
+
+let checkVictory = () => {
+  if (deadCounter >= 10 && lostCounter < 5) {
+    alert("Вы победили!");
+    putin();
+  } else if (lostCounter >= 5) {
+    alert("Вы проиграли!");
+    putin();
+  }
+};
+
+let putin = () => {
+  document.getElementById("dead").innerHTML = 0;
+  document.getElementById("lost").innerHTML = 0;
+  deadCounter = 0;
+  lostCounter = 0;
+};
