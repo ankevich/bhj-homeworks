58456d1329ddd4f7608639d133527bcfd87a7b0d
Annie Bauer <overheads165@gmail.com>
2022-01-22 16:09:23 +0300

    refactoring

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -1,12 +1,10 @@
 let images = Array.from(document.getElementsByClassName("slider__item"));
-
 let prevButton = document.getElementsByClassName("slider__arrow_prev").item(0);
 let nextButton = document.getElementsByClassName("slider__arrow_next").item(0);
 
 let currentSlideNumber = 0;
 
-nextButton.onclick = () => {
-  currentSlideNumber++;
+let setCurrentSlideActive = () => {
   images.map((image, index) => {
     if (index == currentSlideNumber) {
       image.classList.add("slider__item_active");
@@ -14,7 +12,11 @@ nextButton.onclick = () => {
       image.classList.remove("slider__item_active");
     }
   });
+};
 
+nextButton.onclick = () => {
+  currentSlideNumber++;
+  setCurrentSlideActive();
   if (currentSlideNumber >= images.length - 1) {
     currentSlideNumber = 0;
   }
@@ -22,14 +24,7 @@ nextButton.onclick = () => {
 
 prevButton.onclick = () => {
   currentSlideNumber--;
-  images.map((image, index) => {
-    if (index == currentSlideNumber) {
-      image.classList.add("slider__item_active");
-    } else {
-      image.classList.remove("slider__item_active");
-    }
-  });
-
+  setCurrentSlideActive();
   if (currentSlideNumber <= 0) {
     currentSlideNumber = images.length - 1;
   }
