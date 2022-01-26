424cbea16c647e38a234b6c50ce3acaaec2e0d50
Annie Bauer <overheads165@gmail.com>
2022-01-26 18:43:21 +0300

    final dropdown

--- event-object/dropdown/task.js
+++ event-object/dropdown/task.js
@@ -1,14 +1,19 @@
-let dropdownValue = document.getElementsByClassName("dropdown__value").item(0);
-let dropdownList = document.getElementsByClassName("dropdown__list").item(0);
-let dropdownItems = Array.from(document.getElementsByClassName("dropdown__item"));
+let dropdowns = Array.from(document.getElementsByClassName("dropdown"))
 
-dropdownValue.onclick = () => {
-  dropdownList.classList.toggle("dropdown__list_active");
-};
-
-dropdownItems.map((item) => {
-    item.querySelector("a").onclick = () => {
-    dropdownValue.textContent = item.querySelector("a").textContent;
-    return false
+dropdowns.map(dropdown => {
+  let dropdownValue = dropdown.querySelector(".dropdown__value");
+  let dropdownList = dropdown.querySelector(".dropdown__list")
+  let dropdownItems = Array.from(dropdown.querySelectorAll(".dropdown__item"))
+  
+  dropdownValue.onclick = () => {
+    dropdownList.classList.toggle("dropdown__list_active");
   };
-});
+  
+  dropdownItems.map((item) => {
+      item.querySelector("a").onclick = () => {
+      dropdownValue.textContent = item.querySelector("a").textContent;
+      return false
+    };
+  });
+
+})
