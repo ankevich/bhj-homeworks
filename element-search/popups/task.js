b8e929b46e56e74b7c500bfc9ca51776411bdc9c
Annie Bauer <overheads165@gmail.com>
2022-01-26 19:01:10 +0300

    refactoring

--- element-search/popups/task.js
+++ element-search/popups/task.js
@@ -1,23 +1,20 @@
 let modalMain = document.getElementById("modal_main");
 let modalSuccess = document.getElementById("modal_success");
+let closeButtons = Array.from(document.getElementsByClassName("modal__close"));
 
 function showModal() {
   modalMain.classList.add("modal_active");
 }
 showModal();
 
-let closeMain = modalMain.querySelector(".modal__content div.modal__close");
-closeMain.onclick = () => {
-  modalMain.classList.remove("modal_active");
-};
+closeButtons.map((button) => {
+  button.onclick = () => {
+    button.closest(".modal").classList.remove("modal_active");
+  };
+});
 
 let success = modalMain.querySelector("a.show-success");
 success.onclick = () => {
   modalMain.classList.remove("modal_active");
   modalSuccess.classList.add("modal_active");
-};
-
-let closeSuccess = modalSuccess.querySelector(".modal__content div.modal__close");
-closeSuccess.onclick = () => {
-  modalSuccess.classList.remove("modal_active");
 };
