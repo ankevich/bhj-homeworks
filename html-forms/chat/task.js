ca76bb2f286809880329ef4d0f758e25de3e56ef
Annie Bauer <overheads165@gmail.com>
2022-01-28 00:41:14 +0300

    finish

--- html-forms/chat/task.js
+++ html-forms/chat/task.js
@@ -9,11 +9,16 @@ chatWidget.onclick = () => {
 chatWidget.addEventListener("keydown", (event) => {
   if (event.key == "Enter" && chatWidgetInput.value != "") {
     messages.innerHTML += `
-        <div class="message">
+        <div class="message_client">
             <div class="message__time">${timestamp()}</div>
             <div class="message__text">${chatWidgetInput.value}</div>
         </div>
         `;
+    messages.innerHTML += `
+        <div class="message">
+            <div class="message__time">${timestamp()}</div>
+            <div class="message__text">${botMessages[Math.floor(Math.random()*botMessages.length)]}</div>
+        </div>`
   }
 });
 
@@ -21,3 +26,10 @@ const timestamp = () => {
     let now = new Date()
     return now.getHours() + ":" + now.getMinutes()
 }
+
+let botMessages = [
+    "Добрейшего вечера!",
+    "Ваш звонок очень важен для нас",
+    "Прекрасная погода",
+    "Я не должен бояться, страх это маленькая смерть",
+]
