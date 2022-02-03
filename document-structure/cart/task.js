49fc4713f18c80650d5fb78dc1bc507f5acc525f
Annie Bauer <overheads165@gmail.com>
2022-02-03 23:39:51 +0300

    добавление товаров

--- document-structure/cart/task.js
+++ document-structure/cart/task.js
@@ -1,6 +1,12 @@
-let decButtons = Array.from(document.getElementsByClassName("product__quantity-control_dec"));
-let incButtons = Array.from(document.getElementsByClassName("product__quantity-control_inc"));
+let decButtons = Array.from(
+  document.getElementsByClassName("product__quantity-control_dec")
+);
+let incButtons = Array.from(
+  document.getElementsByClassName("product__quantity-control_inc")
+);
 let values = document.getElementsByClassName("product__quantity-value");
+let addButtons = Array.from(document.getElementsByClassName("product__add"));
+let cart = document.querySelector(".cart__products");
 
 incButtons.map((button) => {
   button.onclick = () => {
@@ -20,5 +26,18 @@ decButtons.map((button) => {
     if (value.textContent > 1) {
       value.textContent--;
     }
+  };
+});
+
+addButtons.map((button) => {
+  button.onclick = () => {
+      let product = button.closest(".product")
+      let dataId = product.getAttribute("data-id")
+      let image = product.querySelector(".product__image").src
+      let quantity = product.querySelector(".product__quantity-value").textContent
+    cart.innerHTML += `<div class="cart__product" data-id="${dataId}">
+    <img class="cart__product-image" src="${image}">
+    <div class="cart__product-count">${quantity}</div>
+</div>`;
   };
 });
