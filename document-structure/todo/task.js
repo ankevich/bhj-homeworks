2db349c7afe48e4d33653f3d539a4ded7941e5ce
Annie Bauer <overheads165@gmail.com>
2022-02-04 23:56:16 +0300

    удаление

--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -4,13 +4,20 @@ let addButton = document.getElementById("tasks__add");
 let addText = () => {
   let inputText = document.getElementById("task__input").value;
   if (inputText != "") {
-    tasksList.innerHTML += `<div class="task">
-        <div class="task__title">
-        ${inputText}
-        </div>
-        <a href="#" class="task__remove">&times;</a>
+    tasksList.innerHTML += `
+        <div class="task">
+            <div class="task__title">
+                ${inputText}
+            </div>
+            <a href="#" class="task__remove">&times;</a>
         </div>`;
   }
+  let crosses = Array.from(tasksList.getElementsByClassName("task__remove"));
+  crosses.map((cross) => {
+    cross.onclick = () => {
+      cross.closest(".task").remove()
+    };
+  });
 };
 
 addButton.onclick = () => {
