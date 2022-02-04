89693ca954e1d6720becd9140f0517bd665feb56
Annie Bauer <overheads165@gmail.com>
2022-02-04 22:40:12 +0300

    увеличение счетчика товаров

--- document-structure/cart/task.js
+++ document-structure/cart/task.js
@@ -29,15 +29,40 @@ decButtons.map((button) => {
   };
 });
 
+let addSameProduct = (clickedProductId, quantity) => {
+  let products = Array.from(document.querySelectorAll(".cart__product"))
+  products.map((cartProduct) => {
+    let cartProductDataId = cartProduct.getAttribute("data-id")
+
+    if (clickedProductId == cartProductDataId ) {
+      let count = cartProduct.querySelector(".cart__product-count").textContent
+      let sum = Number(count) + Number(quantity)
+      cartProduct.querySelector(".cart__product-count").textContent = sum
+    }
+  })
+}
+
 addButtons.map((button) => {
   button.onclick = () => {
       let product = button.closest(".product")
+      
       let dataId = product.getAttribute("data-id")
       let image = product.querySelector(".product__image").src
       let quantity = product.querySelector(".product__quantity-value").textContent
-    cart.innerHTML += `<div class="cart__product" data-id="${dataId}">
-    <img class="cart__product-image" src="${image}">
-    <div class="cart__product-count">${quantity}</div>
-</div>`;
+
+      let cartProducts = Array.from(document.querySelectorAll(".cart__product"))
+      let filteredProducts = cartProducts.filter((p) => p.getAttribute("data-id") == dataId)
+      let isCartHasProduct = filteredProducts.length > 0
+
+      if (isCartHasProduct) {
+        addSameProduct(dataId, quantity)
+      } else {
+        cart.innerHTML += `
+          <div class="cart__product" data-id="${dataId}">
+            <img class="cart__product-image" src="${image}">
+            <div class="cart__product-count">${quantity}</div>
+          </div>`;
+      }
+
   };
 });
