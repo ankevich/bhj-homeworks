71be1fdb6d9e2e5a1281b9deab8a3f6bb9f26a31
Annie Bauer <overheads165@gmail.com>
2022-02-05 22:57:58 +0300

    очистка инпута

--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -2,15 +2,15 @@ let tasksList = document.getElementById("tasks__list");
 let addButton = document.getElementById("tasks__add");
 
 let addText = () => {
-  let inputText = document.getElementById("task__input").value;
+  let inputText = document.getElementById("task__input").value.trim();
   if (inputText != "") {
     tasksList.innerHTML += `
-        <div class="task">
-            <div class="task__title">
-                ${inputText}
-            </div>
-            <a href="#" class="task__remove">&times;</a>
-        </div>`;
+      <div class="task">
+          <div class="task__title">
+              ${inputText}
+          </div>
+          <a href="#" class="task__remove">&times;</a>
+      </div>`;
   }
   let crosses = Array.from(tasksList.getElementsByClassName("task__remove"));
   crosses.map((cross) => {
@@ -22,5 +22,6 @@ let addText = () => {
 
 addButton.onclick = () => {
   addText();
+  document.getElementById("task__input").value = ""
   return false;
 };
